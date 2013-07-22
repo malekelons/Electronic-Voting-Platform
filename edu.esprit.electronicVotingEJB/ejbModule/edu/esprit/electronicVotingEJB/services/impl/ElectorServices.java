c10f465f34a8a8411d8ee20d89d3b217de41c0e3
Malek <malekelones.benamor@esprit.tn>
2013-07-22 11:26:08 +0200

    Signed-off-by: Malek <malekelones.benamor@esprit.tn>

--- edu.esprit.electronicVotingEJB/ejbModule/edu/esprit/electronicVotingEJB/services/impl/ElectorServices.java
+++ edu.esprit.electronicVotingEJB/ejbModule/edu/esprit/electronicVotingEJB/services/impl/ElectorServices.java
@@ -1,6 +1,7 @@
 package edu.esprit.electronicVotingEJB.services.impl;
 
 import javax.ejb.Stateless;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
--- edu.esprit.electronicVotingEJB/ejbModule/edu/esprit/electronicVotingEJB/services/interfaces/ElectorServicesRemote.java
+++ edu.esprit.electronicVotingEJB/ejbModule/edu/esprit/electronicVotingEJB/services/interfaces/ElectorServicesRemote.java
@@ -7,7 +7,6 @@ import edu.esprit.electronicVotingEJB.domain.Elector;
 @Remote
 public interface ElectorServicesRemote {
 	public void addElector(Elector elector);
-
 	public void deleteElector(int idElector);
 	public void updateElector(Elector elector);
 	public Elector findElectorById(int idElector);
